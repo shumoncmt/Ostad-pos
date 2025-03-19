d92a9d7da5c8313ff75fce705d706f173485d91d
Md. Shumon Mia <shomoncmt@gmail.com>
2025-03-19 10:15:40 +0600

    update project

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,7 +47,7 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
+            // 'password' => 'hashed',
         ];
     }
     public function categories()
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\TokenVerificationMiddleware;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -9,3 +11,20 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/test', [HomeController::class, 'Test']);
+
+//User all routes
+Route::post('/user-registration', [UserController::class, 'UserRegistration'])->name('user.registration');
+Route::post('/user-login', [UserController::class, 'UserLogin'])->name('user.login');
+Route::post('/send-otp', [UserController::class, 'SendOTPCode'])->name('SendOTPCode');
+Route::post('/verify-otp', [UserController::class, 'VerifyOTP'])->name('VerifyOTP');
+
+
+
+Route::middleware(TokenVerificationMiddleware::class)->group(function(){
+    //reset password
+    Route::post('/reset-password', [UserController::class, 'ResetPassword']);
+
+    Route::get('/DashboardPage', [UserController::class, 'DashboardPage']);
+    Route::get('/user-logout', [UserController::class, 'UserLogout']);
+
+});
